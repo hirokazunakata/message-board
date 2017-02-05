4a13fb989d1a9bdc70da3ec40d87d58794c6f6a6
hirokazunakata <hekomikunn@gmail.com>
2017-02-05 05:16:25 +0000

    Task Challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,7 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence: true
   
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
+  
+  validates :age , numericality:true, inclusion:{in:0..150}, presence: true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170129135638) do
+ActiveRecord::Schema.define(version: 20170205025900) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
